56c9d29e962834dc3e63302ad240db59d495a9a6
The Very Cool School Pc <ddd.com>
2019-05-03 09:33:31 +0100

    exercise 1 finished missing prgram copy from 07 class and correct it

---
+++ ColorBallsWithProperties/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorBallsWithProperties
+{
+    class Ball
+    {
+        // Instance variables
+        private Color color;
+        private float radius;
+        // Propertie of times thrown
+        public int XThrown { get; private set; }
+
+        // Constructor that accepts color and radius and degines xThrown
+        public Ball(Color color, float radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            XThrown = 0;
+        }
+
+        // Method that bursts the ball
+        public void Burst()
+        {
+            radius = 0;
+        }
+
+        // Method that throws the balls
+        public void Yeet()
+        {
+            if (radius > 0)
+            {
+                XThrown++;
+            }
+        }
+    }
+}
---
+++ ColorBallsWithProperties/Color.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorBallsWithProperties
+{
+    class Color
+    {
+        // Properties
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+        // Propertie Grayscale
+        public byte Grayscale => (byte)((Red + Green + Blue) / 3);
+
+        // Constructor
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+        }
+        // Constructor
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+    }
+}
